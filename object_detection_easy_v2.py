29b90214bdec8f6c4bd05ddc180f583c46780bb0
ET Tech <55145361+ET-Technologies@users.noreply.github.com>
2020-07-10 09:54:53 +0200

    Update object_detection_easy_v2.py

--- object_detection_easy_v2.py
+++ object_detection_easy_v2.py
@@ -1,206 +1,149 @@
-%%writefile person_detect.py
-
+#python object_detection_v2.py --model person-detection-retail-0013 --video Manufacturing.mp4
 import numpy as np
 import time
-from openvino.inference_engine import IENetwork, IECore
 import os
 import cv2
 import argparse
 import sys
+from openvino.inference_engine import IENetwork, IECore
 
-
-class Queue:
+class Inference:
     '''
-    Class for dealing with queues
+    Class with all relevant tools to do object detection
     '''
-    def __init__(self):
-        self.queues=[]
-
-    def add_queue(self, points):
-        self.queues.append(points)
-
-    def get_queues(self, image):
-        for q in self.queues:
-            x_min, y_min, x_max, y_max=q
-            frame=image[y_min:y_max, x_min:x_max]
-            yield frame
-    
-    def check_coords(self, coords):
-        d={k+1:0 for k in range(len(self.queues))}
-        for coord in coords:
-            for i, q in enumerate(self.queues):
-                if coord[0]>q[0] and coord[2]<q[2]:
-                    d[i+1]+=1
-        return d
-
-
-class PersonDetect:
-    '''
-    Class for the Person Detection Model.
-    '''
-
+    # Load all relevant variables into the class
     def __init__(self, model_name, device, threshold=0.60):
         self.model_weights=model_name+'.bin'
         self.model_structure=model_name+'.xml'
         self.device=device
         self.threshold=threshold
+        
+        # Initialise the network and save it in the self.model variables
+        self.model=IENetwork(self.model_structure, self.model_weights) # old openvino version
+        #self.model = core.read_network(self.model_structure, self.model_weights) # new openvino version
+        
+        # Get the input layer
+        self.input_name = next(iter(self.model.inputs))
+        self.input_shape = self.model.inputs[self.input_name].shape
+        self.output_name = next(iter(self.model.outputs))
+        self.output_shape = self.model.outputs[self.output_name].shape
 
-        try:
-            # old version self.model=IENetwork(self.model_structure, self.model_weights)
-            self.model = core.read_network(self.model_structure, self.model_weights)
-        except Exception as e:
-            raise ValueError("Could not Initialise the network. Have you enterred the correct model path?")
-
-        self.input_name=next(iter(self.model.inputs))
-        self.input_shape=self.model.inputs[self.input_name].shape
-        self.output_name=next(iter(self.model.outputs))
-        self.output_shape=self.model.outputs[self.output_name].shape
-
-    def load_model(self):
-        
-        core = IECore()
-        self.exec_network = core.load_network(network=self.model, device_name=self.device, num_requests=1)
-        
-    def predict(self, image, initial_w, initial_h):
-        
-        input_img = image
-        # Pre-process the image
-        image = preprocess_input(image)
-        
-        # Perform inference
-        result = async_inference(image)
-        
-        # Draw Bounding Box
-        image, coords = draw_outputs(results, image, initial_w, initial_h)
-
-        return coords, image
+        print ("input_name:" +str(self.input_name))
+        print ("input_shape:" +str(self.input_shape))
+        print ("output_name: " +str(self.output_name))
+        print ("output_shape: " +str(self.output_shape))
     
+    # Loads the model
+    def load_model(self):
+        # Adds Extension
+        CPU_EXTENSION = "/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so"
+        self.core = IECore()
+        self.core.add_extension(CPU_EXTENSION, self.device)
+        # Load the network into an executable network
+        self.exec_network = self.core.load_network(network=self.model, device_name=self.device, num_requests=1)
+        print ("Model is loaded")
+        
+    # Start asyncron inference request and wait for the result
     def async_inference(self, image):
         infer_request_handle = self.exec_network.start_async(request_id=0, inputs={self.input_name: image})
-        
         while True:
             status = self.exec_network.requests[0].wait(-1)
             if status == 0:
-                res = infer_request_handle.outputs[self.output_name]
                 break
             else:
-                time.sleep(1)      
+                time.sleep(1)
+            print ("status: " +str(status))
+            return infer_request_handle
         
+     # Get the inference output
+    def get_output(infer_request_handle, request_id=0, output=None):
+        if output:
+            res = infer_request_handle.output[output]
+        else:
+            res = self.exec_network.requests[request_id].outputs[self.output_name]
         return res
     
-    def draw_outputs(self, coords, image, initial_w, initial_h):
-        
-        det = []
- 
-        for obj in coords[0][0]:
+    # Draw Bounding Box
+    def boundingbox(res, initial_w, initial_h, frame):
+        current_count = 0
+        for obj in res[0][0]:
+            # Draw bounding box for object when it's probability is more than the specified threshold
             if obj[2] > self.threshold:
                 xmin = int(obj[3] * initial_w)
                 ymin = int(obj[4] * initial_h)
                 xmax = int(obj[5] * initial_w)
                 ymax = int(obj[6] * initial_h)
-                cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (0, 55, 255),1)
-                
-                det.append(obj)
-    
-        return image, det
-  
+                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 55, 255), 1)
+                current_count = current_count + 1
+                print ("Current count: " + str(current_count))
+        return frame 
 
-    def preprocess_input(self, image):
-        
-        n, c, h, w = self.input_shape
-        
-        image = cv2.resize(image, (w, h))
-        image = image.transpose((2, 0, 1))
-        image = image.reshape((n, c, h, w))
-        
-        return image
-
-def main(args):
-    
-    model=args.model
-    device=args.device
-    video_file=args.video
-    max_people=args.max_people
-    threshold=args.threshold
-    output_path=args.output_path
-
-    start_model_load_time=time.time()
-    pd= PersonDetect(model, device, threshold)
-    pd.load_model()
-    total_model_load_time = time.time() - start_model_load_time
-
-    queue=Queue()
-    
-    try:
-        queue_param=np.load(args.queue_param)
-        for q in queue_param:
-            queue.add_queue(q)
-    except:
-        print("error loading queue param file")
-
-    try:
-        cap=cv2.VideoCapture(video_file)
-    except FileNotFoundError:
-        print("Cannot locate video file: "+ video_file)
-    except Exception as e:
-        print("Something else went wrong with the video file: ", e)
-    
-    initial_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    initial_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    video_len = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    out_video = cv2.VideoWriter(os.path.join(output_path, 'output_video.mp4'), cv2.VideoWriter_fourcc(*'avc1'), fps, (initial_w, initial_h), True)
-    
-    counter=0
-    start_inference_time=time.time()
-
-    try:
-        while cap.isOpened():
-            ret, frame=cap.read()
-            if not ret:
-                break
-            counter+=1
-            
-            coords, image= pd.predict(frame, initial_w, initial_h)
-            num_people= queue.check_coords(coords)
-            print(f"Total People in frame = {len(coords)}")
-            print(f"Number of people in queue = {num_people}")
-            out_text=""
-            y_pixel=25
-            
-            for k, v in num_people.items():
-                out_text += f"No. of People in Queue {k} is {v} "
-                if v >= int(max_people):
-                    out_text += f" Queue full; Please move to next Queue "
-                cv2.putText(image, out_text, (15, y_pixel), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-                out_text=""
-                y_pixel+=40
-            out_video.write(image)
-            
-        total_time=time.time()-start_inference_time
-        total_inference_time=round(total_time, 1)
-        fps=counter/total_inference_time
-
-        with open(os.path.join(output_path, 'stats.txt'), 'w') as f:
-            f.write(str(total_inference_time)+'\n')
-            f.write(str(fps)+'\n')
-            f.write(str(total_model_load_time)+'\n')
-
-        cap.release()
-        cv2.destroyAllWindows()
-    except Exception as e:
-        print("Could not run Inference: ", e)
-
-if __name__=='__main__':
+# Collect all the necessary input values
+def build_argparser():
     parser=argparse.ArgumentParser()
     parser.add_argument('--model', required=True)
     parser.add_argument('--device', default='CPU')
     parser.add_argument('--video', default=None)
     parser.add_argument('--queue_param', default=None)
-    parser.add_argument('--output_path', default='/results')
+    parser.add_argument('--output_path', default='results/')
     parser.add_argument('--max_people', default=2)
     parser.add_argument('--threshold', default=0.60)
-    
-    args=parser.parse_args()
 
-    main(args)
+    return parser
+
+def main():
+    args = build_argparser().parse_args()
+    model_name=args.model
+    device=args.device
+    video_file=args.video
+    max_people=args.max_people
+    threshold=args.threshold
+    output_path=args.output_path
+    
+    start_model_load_time=time.time() # Time to load the model (Start)
+    # Load class Inference as inference
+    inference= Inference(model_name, device, threshold)
+    # Loads the model from the inference class
+    inference.load_model()
+    total_model_load_time = time.time() - start_model_load_time # Time model needed to load
+    print ("Time to load model: " + str(total_model_load_time))
+    
+    # Get the input video stream
+    cap=cv2.VideoCapture(video_file)
+    
+    # Capture information about the input video stream
+    initial_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    initial_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    video_len = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    
+    print ("initial_w: " +str(initial_w))
+    print ("initial_h: " +str(initial_h))
+    print ("video_len: " +str(video_len))
+    print ("fps: " +str(fps))
+    
+    # Define output video
+    out_video = cv2.VideoWriter(os.path.join(output_path, 'output_video3.mp4'), cv2.VideoWriter_fourcc(*'avc1'), fps, (initial_w, initial_h), True)
+    # We have just one request number is 0
+    request_id=0
+    
+    ### Read from the video capture ###
+    try:
+        while cap.isOpened():
+            ret, frame=cap.read()
+            if not ret:
+                break
+            
+            # Get the image from the inference class
+            image= inference.predict(frame, initial_w, initial_h)
+            # Write the output video
+            out_video.write(image)
+    
+    cap.release()
+    cv2.destroyAllWindows()     
+    
+    
+
+# Start sequence
+if __name__=='__main__':
+    main()
