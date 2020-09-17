086479ddfbb8810858d4d65217f753e401c0dc56
ET Tech <55145361+ET-Technologies@users.noreply.github.com>
2020-09-17 07:51:04 +0200

    Create load_video.py

---
+++ load_video.py
@@ -0,0 +1,53 @@
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import pickle
+import time
+import cv2
+import os
+
+video = 'demos\demo.mp4'
+
+
+def example_videocapture():
+    cap = cv2.VideoCapture(args["video"])
+    while True:
+        ret, frame = cap.read()
+
+        if not cap:
+            break
+
+        frame = imutils.resize(frame, width=450)
+        (h, w) = frame.shape[:2]
+        print (h,w)
+
+        cv2.imshow("Test", frame)
+        cv2.waitKey(1)
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+def example_videostream():
+    cap = VideoStream(usePiCamera=True).start()
+    cap = VideoStream(0).start()
+    # camera warm up
+    time.sleep(2.0)
+    while True:
+        frame = cap.read()
+        frame = imutils.resize(frame, width=450)
+        (h, w) = frame.shape[:2]
+        print (h,w)
+
+        cv2.imshow("Test", frame)
+        cv2.waitKey(1)
+
+    cv2.destroyAllWindows()
+    cap.stop()
+
+
+
+
+
