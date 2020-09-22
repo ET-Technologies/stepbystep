b8e4ae9ae482bad351250f3ffb18447271eac544
ET Tech <55145361+ET-Technologies@users.noreply.github.com>
2020-09-22 19:12:31 +0200

    New

--- OpenCV/imshow.py
+++ OpenCV/imshow.py
@@ -1,8 +1,11 @@
+'''
+python3 imshow.py
+'''
 import cv2
 #from cv2 import waitkey
 path = '/home/thomas/Github/stepbystep/cropped_image.png'
 test = cv2.imread(path)
 window = 'image'
 cv2.imshow(window, test)
-cv2.waitkey(0)
+cv2.waitKey(0)
 cv2.destroyAllWindows() 
