3b4d7505c7fa94c82dca5f65a0aff3a9e7c9c6f9
ET Tech <erwin@entrich-technologies.com>
2020-10-14 19:25:16 +0200

    Update model_optimzer.py

--- Openvino/model_optimzer.py
+++ Openvino/model_optimzer.py
@@ -8,4 +8,9 @@ cd /opt/intel/openvino/deployment_tools/model_optimizer
 python3 mo.py --input_model <INPUT_MODEL>.onnx
 python3 /opt/intel/openvino/deployment_tools/model_optimizer/mo.py --input_model openvino_er_v1.onnx
 python3 /opt/intel/openvino/deployment_tools/model_optimizer/mo.py --input_model /home/thomas/Github/stepbystep/models/openvino_er_v1.onnx
+python3 C:\Program Files (x86)\IntelSWTools\openvino_2020.1.033\deployment_tools\model_optimizer\mo.py --input_model C:\Users\erwin.entrich\OneDrive - Entrich Technologies\Projekte\ET\Github\DC-Box\stepbystep\models\openvino_er_v1.onnx
+C:\Program Files (x86)\IntelSWTools\openvino_2020.1.033\deployment_tools\model_optimizer\mo.py
+
+cd C:\Program Files (x86)\IntelSWTools\openvino_2020.1.033\deployment_tools\model_optimizer\install_prerequisites
+openvino_2020.1.033
 '''
