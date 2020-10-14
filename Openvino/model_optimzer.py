c7c497de63ce282fd1fdffeab30aa5f99d26620d
ET Tech <erwin@entrich-technologies.com>
2020-10-14 14:17:22 +0200

    Update model_optimzer.py

--- Openvino/model_optimzer.py
+++ Openvino/model_optimzer.py
@@ -1,3 +1,7 @@
+
+# Onnx Model
 '''
 cd /opt/intel/openvino/deployment_tools/model_optimizer
+python3 mo.py --input_model <INPUT_MODEL>.onnx
+python3 /opt/intel/openvino/deployment_tools/model_optimizer/mo.py --input_model openvino_er_v1.onnx
 '''
